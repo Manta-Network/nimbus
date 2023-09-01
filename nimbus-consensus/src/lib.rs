e156484607335667fc170bac490b7f144a8c2b2b
Dengjianping <djptux@gmail.com>
2023-09-01 10:39:28 +0800

    Make import_queue module public
    Signed-off-by: Dengjianping <djptux@gmail.com>

--- nimbus-consensus/src/lib.rs
+++ nimbus-consensus/src/lib.rs
@@ -48,6 +48,7 @@ use std::convert::TryInto;
 use std::{marker::PhantomData, sync::Arc, time::Duration};
 use tracing::error;
 mod import_queue;
+pub use import_queue::{import_queue, NimbusBlockImport, Verifier};
 mod manual_seal;
 pub use manual_seal::NimbusManualSealConsensusDataProvider;
 
